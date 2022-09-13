73ae65c64c3d34f9f5d065df939f2bc780c09f20
nullbr <nullbr>
2022-09-13 20:29:44 -0300

    require auth in all routes

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,8 +1,10 @@
+from pyexpat import model
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -12,7 +14,7 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 def create_acesss_token(data: dict):
     to_encode = data.copy()
@@ -40,7 +42,11 @@ def verify_access_token(token: str, credentials_exception):
 
     return token_data
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
 
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    
+    return user
--- app/routers/sale.py
+++ app/routers/sale.py
@@ -11,7 +11,7 @@ router = APIRouter(
 
 
 @router.get("/", status_code=status.HTTP_200_OK, response_model=List[schemas.Sale])
-def sales(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def sales(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM sales """)
     # sales = cursor.fetchall()
     
@@ -21,7 +21,7 @@ def sales(db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_curre
 
 # Path parameter
 @router.get("/{sale_id}", status_code=status.HTTP_200_OK, response_model=schemas.Sale)
-def get_sale(sale_id: int = Path(None, description ="The ID of the desired Sale", gt=0), db: Session = Depends(get_db)):
+def get_sale(sale_id: int = Path(None, description ="The ID of the desired Sale", gt=0), db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM sales WHERE id = %s """, (str(sale_id),))
     # sale = cursor.fetchone()
 
@@ -37,7 +37,7 @@ def get_sale(sale_id: int = Path(None, description ="The ID of the desired Sale"
 
 # Post method
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Sale)
-def create_sale(sale: schemas.CreateSale, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def create_sale(sale: schemas.CreateSale, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO sales (item, price, quantity) VALUES (%s, %s, %s) # RETURNING * """, (sale.item, sale.price, sale.quantity))
     # new_sale = cursor.fetchone()  
     # connection.commit()
@@ -51,7 +51,7 @@ def create_sale(sale: schemas.CreateSale, db: Session = Depends(get_db), user_id
 
 # Put method
 @router.put("/{sale_id}", status_code=status.HTTP_200_OK, response_model=schemas.Sale)
-def update_sale(sale_id: int, sale: schemas.UpdateSale, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def update_sale(sale_id: int, sale: schemas.UpdateSale, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""UPDATE sales SET item = %s, price = %s, quantity = %s WHERE id = %s RETURNING *""", (sale.item, sale.price, sale.quantity, sale_id))
     # updated_sale = cursor.fetchone()
     # connection.commit()
@@ -68,7 +68,7 @@ def update_sale(sale_id: int, sale: schemas.UpdateSale, db: Session = Depends(ge
 
 # Delete method
 @router.delete("/{sale_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_sale(sale_id: int, db: Session = Depends(get_db), user_id: int = Depends(oauth2.get_current_user)):
+def delete_sale(sale_id: int, db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""DELETE FROM sales WHERE id = %s RETURNING *""", (str(sale_id),))
     # deleted_sale = cursor.fetchone()
     # connection.commit()
