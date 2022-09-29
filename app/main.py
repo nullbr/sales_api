b65be17f172ec6b4f573af3e2bcd5bbc296883c6
nullbr <nullbr>
2022-09-28 21:53:04 -0300

    adding cors middleware

--- app/main.py
+++ app/main.py
@@ -2,11 +2,22 @@ from itertools import product
 from fastapi import FastAPI
 from . import models
 from .routers import sale, user, auth, product, product_sold
+from fastapi.middleware.cors import CORSMiddleware
 
 # from .database import engine
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(sale.router)
 app.include_router(user.router)
