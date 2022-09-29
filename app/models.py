4a6381b637bb00e8a57cfc066e7cb68379466192
nullbr <nullbr>
2022-09-28 21:40:50 -0300

    using alembic auto generate feature

--- app/models.py
+++ app/models.py
@@ -11,7 +11,6 @@ class Sale(Base):
     __tablename__ = "sales"
 
     id = Column(Integer, primary_key=True, nullable=False)
-    items = Column(String, nullable=False)
     total = Column(Float, nullable=False)
     payment_method = Column(String, nullable=False)
     credit = Column(Boolean, server_default='False', nullable=False)
